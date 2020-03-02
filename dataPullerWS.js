d7e53f76536c5b536240a5cf1351df75f92b0b2d
subver55 <subver55@gmail.com>
2020-03-02 09:14:40 +0600

    fix

--- dataPullerWS.js
+++ dataPullerWS.js
@@ -18,7 +18,7 @@ function connectWs()
 {
   wsScoreboard = new WebSocket(scoreboardDataWsUrl);
   wsScoreboard.onopen= function(e){
-    var dataSet=["pos","num","competitor_state","name",
+    var dataSet=["pos","pos_change","num","competitor_state","name",
 "best_lap_time","last_lap_time_1","laps_count","diff","last_sector_1","last_sector_2","last_sector_3"];
     wsScoreboard.send(JSON.stringify(dataSet));
   };
